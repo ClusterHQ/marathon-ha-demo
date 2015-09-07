3f78d1dd20cbf9af3f88c94bb045ba6a6df818b9
Kai Davenport <kaiyadavenport@gmail.com>
2015-09-07 14:23:08 +0100

    reset the cache and echo progress creating and tagging nodes

--- createnodes.sh
+++ createnodes.sh
@@ -7,13 +7,18 @@ echo "Launching cluster"
 
 unixsecs=$(date +%s)
 
+export RESET_CACHE=${RESET_CACHE:=""}
 export AMI_ID=${AMI_ID:="ami-55a0c030"}
 export INSTANCE_TYPE=${INSTANCE_TYPE:="c3.xlarge"}
 export DATA_FOLDER=${DATA_FOLDER:="$HOME/.marathon-ha-demo"}
+export PREPEND_TAG=${PREPEND_TAG:="marathon-ha-demo"}
 export AWS_DEFAULT_OUTPUT="json"
 export NODE_NAMES=(master node1 node2)
 
 initialize() {
+  if [[ -n "$RESET_CACHE" ]]; then
+    rm -rf $DATA_FOLDER
+  fi
   mkdir -p $DATA_FOLDER
 }
 
@@ -26,15 +31,19 @@ check-variables() {
 
 create-aws-instances() {
   # create the 3 instances
-  #aws ec2 run-instances --image-id $AMI_ID --count 3 --instance-type $INSTANCE_TYPE --key-name $KEYNAME > $DATA_FOLDER/nodes.json
+  echo "running aws ec2 run-instances for 3 nodes"
+  aws ec2 run-instances --image-id $AMI_ID --count 3 --instance-type $INSTANCE_TYPE --key-name $KEYNAME > $DATA_FOLDER/nodes.json
   # loop over them and assign a Name tag
   local COUNTER=0
-  for i in `cat $DATA_FOLDER/nodes.json | grep InstanceId | cut -d '"' -f4`; do
-    aws ec2 create-tags --resource i-52a17af1 --tags 'Key="Name",Value=master'
-    echo "$i is the ID"
-    local name=${NODE_NAMES[$COUNTER]}
-    echo "$name is the name"
-    echo "counter = $COUNTER"
+  for NODE_ID in `cat $DATA_FOLDER/nodes.json | grep InstanceId | cut -d '"' -f4`; do
+    # extract the name from the array of node names
+    local NODE_NAME=${NODE_NAMES[$COUNTER]}
+    echo "creating name tag for $NODE_NAME - ($NODE_ID)"
+    # create the tag for the node name
+    aws ec2 create-tags --resource $NODE_ID --tags "Key=\"Name\",Value=${PREPEND_TAG}-${NODE_NAME}"
+    # keep a local cache of values for this node
+    echo "id: $NODE_ID" > $DATA_FOLDER/$NODE_NAME.txt
+    # counter to map id onto hostname
     COUNTER=$[$COUNTER +1]
   done
 }
