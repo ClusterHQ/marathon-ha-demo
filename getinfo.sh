cf9ea15a0e69e194f17af4329e13e26a9ecd0b78
Kai Davenport <kaiyadavenport@gmail.com>
2015-09-08 14:11:45 +0100

    print out IP addresses for the nodes and other info

---
+++ getinfo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/vars.sh
+source $DIR/helpers.sh
+
+MASTER_PUBLIC=$(get-public-ip-from-name master)
+NODE1_PUBLIC=$(get-public-ip-from-name node1)
+NODE2_PUBLIC=$(get-public-ip-from-name node2)
+
+echo "Marathon GUI: http://$MASTER_PUBLIC:8080"
+echo "Mesos GUI: http://$MASTER_PUBLIC:5050"
+echo "Node 1 IP: $NODE1_PUBLIC"
+echo "Node 2 IP: $NODE2_PUBLIC"
