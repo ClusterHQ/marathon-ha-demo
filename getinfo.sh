dbf1c35dddb9cb5202eabad44499445796083fed
Kai Davenport <kaiyadavenport@gmail.com>
2015-09-14 14:55:20 +0100

    wrap the info in nice segmentation

--- getinfo.sh
+++ getinfo.sh
@@ -11,7 +11,9 @@ MASTER_PUBLIC=$(get-public-ip-from-name master)
 NODE1_PUBLIC=$(get-public-ip-from-name node1)
 NODE2_PUBLIC=$(get-public-ip-from-name node2)
 
+echo "-----------------------------------------"
 echo "Marathon GUI: http://$MASTER_PUBLIC:8080"
 echo "Mesos GUI: http://$MASTER_PUBLIC:5050"
 echo "Node 1 IP: $NODE1_PUBLIC"
 echo "Node 2 IP: $NODE2_PUBLIC"
+echo "-----------------------------------------"
