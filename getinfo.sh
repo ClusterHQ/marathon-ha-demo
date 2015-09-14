b3174748abefc4fab55214ad454524243472b731
Kai Davenport <kaiyadavenport@gmail.com>
2015-09-14 14:57:44 +0100

    check that the cluster exists before trying to output information about it

--- getinfo.sh
+++ getinfo.sh
@@ -11,9 +11,15 @@ MASTER_PUBLIC=$(get-public-ip-from-name master)
 NODE1_PUBLIC=$(get-public-ip-from-name node1)
 NODE2_PUBLIC=$(get-public-ip-from-name node2)
 
-echo "-----------------------------------------"
-echo "Marathon GUI: http://$MASTER_PUBLIC:8080"
-echo "Mesos GUI: http://$MASTER_PUBLIC:5050"
-echo "Node 1 IP: $NODE1_PUBLIC"
-echo "Node 2 IP: $NODE2_PUBLIC"
-echo "-----------------------------------------"
+if [[ "$MASTER_PUBLIC" == "None" ]]; then
+    >&2 echo "The master node cannot be found - please run 'make cluster' to create the required resources"
+    exit 1
+else
+    echo "-----------------------------------------"
+    echo "Marathon GUI: http://$MASTER_PUBLIC:8080"
+    echo "Mesos GUI: http://$MASTER_PUBLIC:5050"
+    echo "Node 1 IP: $NODE1_PUBLIC"
+    echo "Node 2 IP: $NODE2_PUBLIC"
+    echo "-----------------------------------------"
+fi
+
