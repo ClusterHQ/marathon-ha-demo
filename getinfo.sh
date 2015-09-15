86bb603b72aba120a665857e3844d8c1453e95a1
Kai Davenport <kaiyadavenport@gmail.com>
2015-09-15 12:48:00 +0100

    output private IPs as well as public ones for getinfo

--- getinfo.sh
+++ getinfo.sh
@@ -10,15 +10,18 @@ source $DIR/helpers.sh
 MASTER_PUBLIC=$(get-public-ip-from-name master)
 NODE1_PUBLIC=$(get-public-ip-from-name node1)
 NODE2_PUBLIC=$(get-public-ip-from-name node2)
+MASTER_PRIVATE=$(get-private-ip-from-name master)
+NODE1_PRIVATE=$(get-private-ip-from-name node1)
+NODE2_PRIVATE=$(get-private-ip-from-name node2)
 
 if [[ "$MASTER_PUBLIC" == "None" ]]; then
     >&2 echo "The master node cannot be found - please run 'make cluster' to create the required resources"
     exit 1
 else
     echo "-----------------------------------------"    
-    echo "Master IP: $MASTER_PUBLIC"
-    echo "Node 1 IP: $NODE1_PUBLIC"
-    echo "Node 2 IP: $NODE2_PUBLIC"
+    echo "Master IPs: $MASTER_PUBLIC - $MASTER_PRIVATE"
+    echo "Node 1 IPs: $NODE1_PUBLIC - $NODE1_PRIVATE"
+    echo "Node 2 IPs: $NODE2_PUBLIC - $NODE2_PRIVATE"
     echo "-----------------------------------------"
     echo "Marathon GUI: http://$MASTER_PUBLIC:8080"
     echo "Mesos GUI: http://$MASTER_PUBLIC:5050"
