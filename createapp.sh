9bb590b0be896bf746ed0df7f7d0ffbe53640167
Stephen Nguyen <ClusterHQ@Stephens-MacBook-Pro.local>
2015-09-29 12:00:54 +0100

    Don't output from curl command just print info

--- createapp.sh
+++ createapp.sh
@@ -13,13 +13,14 @@ MASTER_IP=$(get-public-ip-from-name master)
 LOAD_BALANCER_DNS=$(get-load-balancer-dns)
 
 cmd="curl -i -H 'Content-type: application/json' --data @app.json http://$MASTER_IP:8080/v2/groups"
-echo $cmd
-exec $cmd
 echo "----------------------------------------"
-echo "Application uploaded"
+echo "Application uploading"
 echo "You can view it's status in the Marathon GUI"
 echo "You can open the application using the Load Balancer"
 echo "-----------------------------------------"
-echo "Marathon GUI:  http://$MASTER_PUBLIC:8080"
+echo "Marathon GUI:  http://$MASTER_IP:8080"
 echo "Load Balancer: $LOAD_BALANCER_DNS"
 echo "-----------------------------------------"
+echo $cmd
+# exec $cmd 
+exec $cmd >/dev/null 2>&1 
